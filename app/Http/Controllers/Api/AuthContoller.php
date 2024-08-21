50847369511650dcbadd2cde268c47f4b8b1a14c
taha triqui <psiko769@gmail.com>
2024-08-21 17:12:33 +0100

    new commit

---
+++ app/Http/Controllers/Api/AuthContoller.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthContoller extends Controller
+{
+     public function login(Request $request){
+    	$validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (! $token = auth()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->createNewToken($token);
+    }
+       public function logout() {
+        auth()->logout();
+
+        return response()->json(['message' => 'User successfully signed out']);
+    }
+      public function refresh() {
+        return $this->createNewToken(auth()->refresh());
+    }
+
+        public function userProfile() {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthContoller;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,14 @@ Route::get('/product',[ProductController::class, 'index'] );
 Route::get('/show/{id}',[ProductController::class, 'show'] );
 Route::post('/product',[ProductController::class, 'store'] );
 Route::post('/product/{id}',[ProductController::class, 'update'] );
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthContoller::class, 'login']);
+    Route::post('/register', [AuthContoller::class, 'register']);
+    Route::post('/logout', [AuthContoller::class, 'logout']);
+    Route::post('/refresh', [AuthContoller::class, 'refresh']);
+    Route::get('/user-profile', [AuthContoller::class, 'userProfile']); });
